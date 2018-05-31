cd564f87d9869d62590260b7c4d47cff2e92c2e4
mark <murimimg180@gmail.com>
2018-05-31 09:46:53 +0300

    created the welcome page

--- shoes/settings.py
+++ shoes/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'brands',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
